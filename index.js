18308fbc763b6e3cd6024cc5a104e442e293ff51
Nickon Fath-Ordoubadi <nickonfo@gmail.com>
2018-07-02 13:33:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name){
+  `Happy holidays, ${name}!`
+}
+function happyHolidayTo(holiday, name) {
+  `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(days, holiday) {
+  `It's ${days} days untill ${holiday}!`
+}
